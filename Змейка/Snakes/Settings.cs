e248be434f82952b67005ed30c48cc77ef2d1c04
matiksiatik <matvey2005tanki@gmail.com>
2024-06-19 18:35:37 +0300

    Add files via upload

---
+++ Змейка/Snakes/Settings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Settings
+    {
+
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+
+        public static string directions;
+
+        public Settings()
+        {
+            Width = 40;
+            Height = 40;
+            directions = "left";
+        }
+
+
+    }
+}
